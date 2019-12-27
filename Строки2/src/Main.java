3a26e7b8378b28d058501e0d11a05e3ec70a72ec
MrPelemeshka <36153403+MrPelemeshka@users.noreply.github.com>
2019-12-27 17:29:52 +0300

    Add files via upload

--- Строки2/src/Main.java
+++ Строки2/src/Main.java
@@ -7,22 +7,26 @@ public class Main{
         int length = str.length();
         int count = 0;
         int number = 0;
+        int l=0;
         for(int i=0;i<length;i++) {
             if (str.charAt(i) != ' ') {
                 count++;
                 if (count == 3 && i==length-1){
                     number++;
+                    l++;
                     System.out.println(number);
                 }
             }
             else if (count == 3 ) {
                 count = 0;
                 number++;
+                l++;
                 System.out.println(number);
             } else {
                 count = 0;
                 number++;
             }
         }
+        if(l == 0)System.out.println("Таких нет");
     }
 }
